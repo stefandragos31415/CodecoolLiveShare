922b3628f9e29df366144156f29d8e48544211c1
stefandragos31415 <stefandragos31415@gmail.com>
2021-04-27 19:01:20 +0100

    Added "PhonePicker" project

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import useAnnualTax from "./useAnnualTax";
-import Student from "./Student";
+import Phone from "./Phone";
 
 function App() {
+	return <Phone />;
+
 	// function getStudents() {
 	//   axios.get('http://apps.loopevo.com/apis/students/students.php')
 	//   .then((response) => {
@@ -14,32 +15,32 @@ function App() {
 	//     console.log(error);
 	//   })
 	// }
-	const [data, setData] = useState([]);
+	// const [data, setData] = useState([]);
 
-	async function getStudents() {
-		const response = await axios("http://apps.loopevo.com/apis/students/students.php");
-		console.log(response.data);
-		setData(response.data);
-	}
+	// async function getStudents() {
+	// 	const response = await axios("http://apps.loopevo.com/apis/students/students.php");
+	// 	console.log(response.data);
+	// 	setData(response.data);
+	// }
 
-	useEffect(() => {
-		getStudents();
-	}, []);
+	// useEffect(() => {
+	// 	getStudents();
+	// }, []);
 
-	if (data.length === 0) {
-		return <h1>Loading</h1>;
-	}
+	// if (data.length === 0) {
+	// 	return <h1>Loading</h1>;
+	// }
 
-	return (
-		<div className="App">
-			<h1>Students list:</h1>
-			<ul>
-				{data.map((student) => (
-					<Student data={student} key={student.id} />
-				))}
-			</ul>
-		</div>
-	);
+	// return (
+	// 	<div className="App">
+	// 		<h1>Students list:</h1>
+	// 		<ul>
+	// 			{data.map((student) => (
+	// 				<Student data={student} key={student.id} />
+	// 			))}
+	// 		</ul>
+	// 	</div>
+	// );
 }
 
 export default App;
--- ReactApp/src/useAnnualTax.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function useAnnualTax(student) {
-    if (student.annual_tax === '1') {
-        return true
-    } else {
-       return false
-    }
-}
