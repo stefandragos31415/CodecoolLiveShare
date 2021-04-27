5b6219feb57d8328b7a48daddb7eef61ab903045
stefandragos31415 <stefandragos31415@gmail.com>
2021-04-27 16:02:02 +0100

    added folder "_sources" to LiveShare ignore/hide list

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -1,27 +1,26 @@
-import "./App.css"
-import { Route, Link, Switch, HashRouter} from "react-router-dom";
+import "./App.css";
+import { Route, Link, Switch, HashRouter } from "react-router-dom";
 import Home from "./pages/home";
 import Recipe from "./pages/recipes";
 
 function App() {
-    return (
-        <div>
-            <HashRouter>
-              <nav className="navbar">
-                <Link to="/">Recipes</Link>
-                <Link to="/recipe/1">Perfection Salad</Link>
-                <Link to="/recipe/2">Hamburger Pie</Link>
-                <Link to="/recipe/3">Cherry Pineapple Bologna</Link>
-              </nav>
-
-              <Switch>
-                <Route path="/recipe/:recipeId" component={Recipe}></Route>
-                <Route path="/" component={Home}></Route>
-              </Switch>
-            </HashRouter>
-        </div>)
-
-
+	return (
+		<div>
+			<HashRouter>
+				<nav className="navbar">
+					<Link to="/">Recipes</Link>
+					<Link to="/recipe/1">Perfection Salad</Link>
+					<Link to="/recipe/2">Hamburger Pie</Link>
+					<Link to="/recipe/3">Cherry Pineapple Bologna</Link>
+				</nav>
+				s
+				<Switch>
+					<Route path="/recipe/:recipeId" component={Recipe}></Route>
+					<Route path="/" component={Home}></Route>
+				</Switch>
+			</HashRouter>
+		</div>
+	);
 }
 
 export default App;
